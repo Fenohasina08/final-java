23a6554e6012b53aedbd0b0871a538329dccf702
fenohasina <fenohasinaherimamy@gmail.com>
2025-09-03 17:27:25 +0300

    initiation de Junit

---
+++ src/test/java/Grouptest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Grouptest {
+
+    @Test
+    void testExemple() {
+        int a = 5;
+        int b = 5;
+        assertEquals(a, b);  // Vérifie que a == b
+    }
+}
